a1a31cac6eb6284dd86e87e671df0561b9de0bdb
Jônatas Freire <dev@jonatasfreire.com.br>
2020-04-24 17:18:49 -0300

    feat: Customize storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,16 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  colorSecondary: '#1e83b4',
+  appBg: '#0b0a0d',
+  appContentBg: '#2a2730',
+  appBorderColor: '#1e83b4',
+  textColor: '#fff',
+  barTextColor: '#ffffff',
+  barSelectedColor: '#ffffff',
+  barBg: '#1b1a1f',
+  brandTitle: 'ModalGR',
+//   brandImage: 'https://modalgr.com.br/img/logo.png',
+  brandUrl: 'https://modalgr.com.br',
+});
